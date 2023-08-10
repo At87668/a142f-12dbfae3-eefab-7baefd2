057c26901ef76ccb4810ddb93e483d6021beec8d
Author87668 <105369287+At87668@users.noreply.github.com>
2023-08-10 16:51:26 +0800

    Update and rename Mobile.js to Judgment.js

--- OptiNotFine/JavaScripts/Mobile.js
+++
@@ -1,9 +0,0 @@
-// 判断语句
-if (
-  navigator.userAgent.match(/Mobi/i) ||
-  navigator.userAgent.match(/Android/i) ||
-  navigator.userAgent.match(/iPhone/i)
-) {
-  // 如果返回为True执行的操作
-  window.location.href="./Mobile";
-}
