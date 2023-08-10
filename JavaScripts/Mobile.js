950f9705c23092cd4f214d4d7cb9a9dc2e84a93f
Author87668 <105369287+At87668@users.noreply.github.com>
2023-08-10 16:39:32 +0800

    Create Mobile.js

---
+++ JavaScripts/Mobile.js
@@ -0,0 +1,9 @@
+// 判断语句
+if (
+  navigator.userAgent.match(/Mobi/i) ||
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/iPhone/i)
+) {
+  // 如果返回为True执行的操作
+  // alert("这是一个演示语句");
+}
