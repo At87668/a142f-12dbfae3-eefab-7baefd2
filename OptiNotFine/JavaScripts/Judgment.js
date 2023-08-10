d35bd4f3bdef30c45e28b7be5159bf165fc6356b
Author87668 <105369287+At87668@users.noreply.github.com>
2023-08-10 16:52:08 +0800

    Update Judgment.js

--- OptiNotFine/JavaScripts/Judgment.js
+++ OptiNotFine/JavaScripts/Judgment.js
@@ -1,10 +1,10 @@
-var system ={};  
-  var p = navigator.platform;       
-    system.win = p.indexOf("Win") == 0;  
-    system.mac = p.indexOf("Mac") == 0;  
-    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);     
-    if(system.win||system.mac||system.xll){
-      window.location.href="../Computer.html";  
-    }else{
-      window.location.href="../Mobile.html";  
-    }
+var system ={};
+var p = navigator.platform;
+system.win = p.indexOf("Win") == 0;
+system.mac = p.indexOf("Mac") == 0;
+system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+if(system.win||system.mac||system.xll){
+  window.location.href="../Computer.html";
+}else{
+  window.location.href="../Mobile.html";
+}
